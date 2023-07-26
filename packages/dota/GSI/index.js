e2bfe98225c1605bf70e8fa81fdf1a6bccb20f1a
kien <akitak1290@gmail.com>
2023-07-26 04:22:29 -0400

    feat: add player join match detection

--- packages/dota/GSI/index.js
+++ packages/dota/GSI/index.js
@@ -1,27 +1,83 @@
 import d2gsi from 'dota2-gsi';
-const server = new d2gsi();
+import EventEmitter from 'events';
+import logger from '../utils/logger.js';
 
-// let client_store;
-// const player_ids = [];
+class GSIServer extends EventEmitter {
+	constructor() {
+		super();
+		// start express server
+		this.server = new d2gsi();
+		this._inGame = false;
+		this._liveGameWatcher = -1;
 
-server.events.on('newclient', function(client) {
-	console.log('New client connection, IP address: ' + client.ip);
-	if (client.auth && client.auth.token) {
-		console.log('Auth token: ' + client.auth.token);
+		this.server.events.on('newclient', (client) => {
+			logger.info(`[GSISERVER] New client connection, IP address: ${client.ip}`);
+			if (client.auth && client.auth.token) {
+				logger.info(`[GSISERVER] Auth token: ${client.auth.token}`);
+			}
+			else {
+				logger.warn('[GSISERVER] No Auth token');
+			}
+
+			// eslint-disable-next-line no-unused-vars
+			client.on('map:clock_time', (_) => {
+				if (!this._inGame) {
+					this._inGame = true;
+					this.emit('getLiveMatchData');
+				}
+
+				clearTimeout(this._liveGameWatcher);
+
+				// reset state in case player is disconnected
+				// or the game is finished
+				this._liveGameWatcher = setTimeout(() => {
+					this._inGame = false;
+				}, 5 * 1000);
+			});
+
+			// eslint-disable-next-line no-unused-vars
+			// client.on('map:clock_time', function(_) {
+			// 	let client_store;
+
+			//	// ! gamestate.player.team only works in spectator mode
+			// 	const player_ids = [];
+
+			// 	const initalSetup = setInterval(function() {
+			// 		// exit the current game
+			// 		if (!client.gamestate.player.team2) {
+			// 			clearInterval(initalSetup);
+			// 		}
+
+			// 		// players ids found
+			// 		if (client.gamestate.player.team2 && player_ids.length === 0) {
+			// 			for (const player in client.gamestate.player.team2) {
+			// 				player_ids.push(client.gamestate.player.team2[player].accountid);
+			// 			}
+			// 			for (const player in client.gamestate.player.team3) {
+			// 				player_ids.push(client.gamestate.player.team3[player].accountid);
+			// 			}
+			// 		}
+
+			// 		// stored players ids
+			// 		if (player_ids.length == 10) {
+			// 			clearInterval(initalSetup);
+			// 			console.log(player_ids);
+			// 		}
+			// 	}, 10 * 1000);
+
+			// 	stop finding players ids after 2 minutes
+			// 	setTimeout(() => {
+			// 		clearInterval(initalSetup);
+			// 	}, 2 * 60 * 1000);
+			// });
+
+			client.on('player:activity', function(activity) {
+				if (activity == 'playing') console.log('Game started!');
+			});
+		});
 	}
-	else {
-		console.log('No Auth token');
-	}
+}
 
-	client.on('player:activity', function(activity) {
-		if (activity == 'playing') console.log('Game started!');
-	});
-	client.on('hero:level', function(level) {
-		console.log('Now level ' + level);
-	});
-	client.on('abilities:ability0:can_cast', function(can_cast) {
-		if (can_cast) console.log('Ability0 off cooldown!');
-	});
-});
+const gsiServerSingleton = new GSIServer();
 
-export default server;
+export default gsiServerSingleton;

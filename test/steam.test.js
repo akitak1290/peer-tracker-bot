a86ba37eddd6e5dbcafe6113c0386a9085bd18d1
kien <akitak1290@gmail.com>
2023-07-28 15:01:32 -0400

    chore: cleanup

--- test/steam.test.js
+++ test/steam.test.js
@@ -1,16 +1,19 @@
 import steamClientSingleton from '../packages/dota/steam/index.js';
 import { ErrorDotaClient } from '../packages/dota/utils/const.js';
 
-test('testing game coordinator', async () => {
-	steamClientSingleton.connect();
+describe('steam client test', () => {
+	test('testing game coordinator', async () => {
+		expect.assertions(1);
+		steamClientSingleton.connect();
 
-	try {
-		await steamClientSingleton.requestSpectateFriendGame(12131);
-	}
-	catch (error) {
-		expect(error).toMatch(ErrorDotaClient[1]);
-		steamClientSingleton.exit();
-	}
-// the request will fail after 190 seconds and 9 attempts
+		try {
+			await steamClientSingleton.requestSpectateFriendGame(12131);
+		}
+		catch (error) {
+			expect(error).toMatch(ErrorDotaClient[1]);
+			steamClientSingleton.exit();
+		}
+	// the request will fail after 190 seconds and 9 attempts
+	}, 240 * 1000);
 });
 
